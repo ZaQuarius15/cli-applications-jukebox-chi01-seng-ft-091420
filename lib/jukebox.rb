2d0c09f789a13d40dc779606b1ee9c7fbc8db70c
Zack Carlson <jzcarlson014@gmail.com>
2020-09-10 02:29:27 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -20,12 +20,8 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   song_name = gets.strip
-  if songs.include? (song_name)
-    puts "Playing #{song_name}"
-  elsif (1..9).to_a.include? (song_name_to.i)
-    puts "Playing #{songs[song_name.to_i - 1]}"
-  else
-    puts "Invalid input, please try again"
+  songs.each_with_index do |song, index|
+    binding.pry
   end
 end
 
